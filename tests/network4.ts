8bcfa5f7b28aa857e01c05891fd12c9016b4f809
Terseus <terseus@gmail.com>
2017-11-15 22:32:49 +0100

    Refactor `Network4` tests to separate different tests

--- tests/network4.ts
+++ tests/network4.ts
@@ -5,6 +5,7 @@ import {
     Network4,
 } from "../src";
 
+
 interface NetworkInfo {
     stringAddress: string;
     octetsAddress: number[];
@@ -16,8 +17,8 @@ interface NetworkInfo {
     stringBroadcast: string;
     octetsBroadcast: number[];
     numAddresses: number;
-    inputPrefix?: string;
-    inputAddress?: string;
+    inputPrefix: string;
+    inputAddress: string;
 }
 
 
@@ -64,59 +65,125 @@ const IPV4_NETWORKS_VALID: NetworkInfo[] = (() => {
 })();
 
 
-function assertNetworkCheck(net: Network4, data: NetworkInfo) {
-    assert(net instanceof Network);
-    assert(net.getAddress() instanceof Address4);
-    assert.equal(net.getAddress().getIpString(), data.stringAddress);
-    assert.deepEqual(net.getAddress().getOctets(), data.octetsAddress);
-    assert(net.getNetmask() instanceof Address4);
-    assert.equal(net.getNetmask().getIpString(), data.stringNetmask);
-    assert.deepEqual(net.getNetmask().getOctets(), data.octetsNetmask);
-    assert.equal(net.getPrefix(), data.prefix);
-    assert(net.getHostmask() instanceof Address4);
-    assert.equal(net.getHostmask().getIpString(), data.stringHostmask);
-    assert.deepEqual(net.getHostmask().getOctets(), data.octetsHostmask);
-    assert(net.getBroadcast() instanceof Address4);
-    assert.equal(net.getBroadcast().getIpString(), data.stringBroadcast);
-    assert.deepEqual(net.getBroadcast().getOctets(), data.octetsBroadcast);
-    assert.equal(net.getNumAddresses(), data.numAddresses);
-    assert.equal(net.hosts().next().value.getIpString(), new Address4(data.octetsAddress).add([1]).getIpString());
-    assert(
-        net.hosts(new Address4(data.octetsBroadcast).substract([1])).next().done,
-        "Last IP check failed: " + new Address4(data.octetsBroadcast).substract([1]).getIpString(),
-    );
-    assert(
-        net.contains(new Address4(data.octetsAddress).add([1])),
-        "First IP contains failed: " + new Address4(data.octetsAddress).add([1]).getIpString(),
-    );
-    assert(
-        net.contains(new Address4(data.octetsBroadcast).substract([1])),
-        "Last IP contains failed: " + new Address4(data.octetsBroadcast).substract([1]).getIpString(),
-    );
-    assert(
-        !net.contains(new Address4(data.octetsAddress).substract([1])),
-        "Before first IP contains failed: " + new Address4(data.octetsAddress).substract([1]).getIpString(),
-    );
-    assert(
-        !net.contains(new Address4(data.octetsBroadcast).add([1])),
-        "After last IP contains failed: " + new Address4(data.octetsBroadcast).add([1]).getIpString(),
-    );
+function testNetwork(check: (net: Network4) => void) {
+    IPV4_NETWORKS_VALID.forEach((data) => {
+        check(new Network4(data.inputPrefix));
+        check(new Network4(data.inputAddress));
+    });
+}
+
+
+function describeTestNetwork(describeSuffix: string, inputAccessor: (data: NetworkInfo) => string) {
+    describe(`public interface ${describeSuffix}`, function() {
+        it("should have the correct types", function() {
+            IPV4_NETWORKS_VALID.forEach((data) => {
+                const net = new Network4(inputAccessor(data));
+                assert(net instanceof Network);
+                assert(net.getAddress() instanceof Address4);
+                assert(net.getNetmask() instanceof Address4);
+                assert(net.getHostmask() instanceof Address4);
+                assert(net.getBroadcast() instanceof Address4);
+            });
+        });
+        it("should match network address", function() {
+            IPV4_NETWORKS_VALID.forEach((data) => {
+                const net = new Network4(inputAccessor(data));
+                assert.equal(net.getAddress().getIpString(), data.stringAddress);
+                assert.deepEqual(net.getAddress().getOctets(), data.octetsAddress);
+            });
+        });
+        it("should match netmask address", function() {
+            IPV4_NETWORKS_VALID.forEach((data) => {
+                const net = new Network4(inputAccessor(data));
+                assert.equal(net.getNetmask().getIpString(), data.stringNetmask);
+                assert.deepEqual(net.getNetmask().getOctets(), data.octetsNetmask);
+            });
+        });
+        it("should match prefix", function() {
+            IPV4_NETWORKS_VALID.forEach((data) => {
+                const net = new Network4(inputAccessor(data));
+                assert.equal(net.getPrefix(), data.prefix);
+            });
+        });
+        it("should match hostmask address", function() {
+            IPV4_NETWORKS_VALID.forEach((data) => {
+                const net = new Network4(inputAccessor(data));
+                assert.equal(net.getHostmask().getIpString(), data.stringHostmask);
+                assert.deepEqual(net.getHostmask().getOctets(), data.octetsHostmask);
+            });
+        });
+        it("should match broadcast address", function() {
+            IPV4_NETWORKS_VALID.forEach((data) => {
+                const net = new Network4(inputAccessor(data));
+                assert.equal(net.getBroadcast().getIpString(), data.stringBroadcast);
+                assert.deepEqual(net.getBroadcast().getOctets(), data.octetsBroadcast);
+            });
+        });
+        it("should match number of addresses", function() {
+            IPV4_NETWORKS_VALID.forEach((data) => {
+                const net = new Network4(inputAccessor(data));
+                assert.equal(net.getNumAddresses(), data.numAddresses);
+            });
+        });
+        it("hosts should start with the first address", function() {
+            IPV4_NETWORKS_VALID.forEach((data) => {
+                const net = new Network4(inputAccessor(data));
+                assert.equal(
+                    net.hosts().next().value.getIpString(),
+                    new Address4(data.octetsAddress).add([1]).getIpString(),
+                );
+            });
+        });
+        it("hosts should end with the last address", function() {
+            IPV4_NETWORKS_VALID.forEach((data) => {
+                const net = new Network4(inputAccessor(data));
+                assert(net.hosts(new Address4(data.octetsBroadcast).substract([1])).next().done);
+            });
+        });
+        it("should contains the first address", function() {
+            IPV4_NETWORKS_VALID.forEach((data) => {
+                const net = new Network4(inputAccessor(data));
+                assert(net.contains(new Address4(data.octetsAddress).add([1])));
+            });
+        });
+        it("should contains the last address", function() {
+            IPV4_NETWORKS_VALID.forEach((data) => {
+                const net = new Network4(inputAccessor(data));
+                assert(net.contains(new Address4(data.octetsBroadcast).substract([1])));
+            });
+        });
+        it("should NOT contains before the first address", function() {
+            IPV4_NETWORKS_VALID.forEach((data) => {
+                const net = new Network4(inputAccessor(data));
+                assert(!net.contains(new Address4(data.octetsAddress).substract([1])));
+            });
+        });
+        it("should NOT contains after the last address", function() {
+            IPV4_NETWORKS_VALID.forEach((data) => {
+                const net = new Network4(inputAccessor(data));
+                assert(!net.contains(new Address4(data.octetsBroadcast).add([1])));
+            });
+        });
+    });
+
 }
 
 
 describe("Network4", function() {
     describe("constructor with prefix netmask", function() {
         it("should accept valid values", function() {
-            IPV4_NETWORKS_VALID.forEach(
-                (data) => assertNetworkCheck(new Network4(data.inputPrefix as string), data),
-            );
+            IPV4_NETWORKS_VALID.forEach((data) => {
+                const net = new Network4(data.inputPrefix);
+            });
         });
     });
     describe("constructor with address netmask", function() {
         it("should accept valid values", function() {
-            IPV4_NETWORKS_VALID.forEach(
-                (data) => assertNetworkCheck(new Network4(data.inputAddress as string), data),
-            );
+            IPV4_NETWORKS_VALID.forEach((data) => {
+                const net = new Network4(data.inputAddress);
+            });
         });
     });
+    describeTestNetwork("from prefix", (data: NetworkInfo) => data.inputPrefix);
+    describeTestNetwork("from address", (data: NetworkInfo) => data.inputAddress);
 });
