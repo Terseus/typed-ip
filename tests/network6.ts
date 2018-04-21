e95a2f081dbb85d618b6d3c9eff9363924ad1edd
Terseus <terseus@gmail.com>
2018-04-21 10:00:35 +0200

    Make Network6 single-assert, remove unnecessary tests
    The base functionality it's already in `Network4`, we might re-add them
    if they prove useful.

--- tests/network6.ts
+++ tests/network6.ts
@@ -1,4 +1,4 @@
-import * as assert from "assert";
+import { assert } from "chai";
 import {
     Address6,
     Network,
@@ -9,14 +9,10 @@ import {
 interface NetworkInfo {
     input: string;
     prefix: number;
-    stringAddress: string;
-    octetsAddress: number[];
-    stringNetmask: string;
-    octetsNetmask: number[];
-    stringWildcard: string;
-    octetsWildcard: number[];
-    stringBroadcast: string;
-    octetsBroadcast: number[];
+    address: number[];
+    netmask: number[];
+    wildcard: number[];
+    broadcast: number[];
 }
 
 
@@ -24,118 +20,53 @@ interface NetworkInfo {
 const IPV6_NETWORKS_VALID: NetworkInfo[] = [{
     input: "0001:0:0:0:0:1:0:0/32",
     prefix: 32,
-    stringAddress: "1::1:0:0",
-    octetsAddress: [0, 1, 0, 0, 0, 0, 0, 0,
-                    0, 0, 0, 1, 0, 0, 0, 0],
-    stringNetmask: "ffff:ffff::",
-    octetsNetmask: [255, 255, 255, 255, 0, 0, 0, 0,
-                    0, 0, 0, 0, 0, 0, 0, 0],
-    stringWildcard: "::ffff:ffff:ffff:ffff:ffff:ffff",
-    octetsWildcard: [0, 0, 0, 0, 255, 255, 255, 255,
-                     255, 255, 255, 255, 255, 255, 255, 255],
-    stringBroadcast: "1:0:ffff:ffff:ffff:ffff:ffff:ffff",
-    octetsBroadcast: [0, 1, 0, 0, 255, 255, 255, 255,
-                      255, 255, 255, 255, 255, 255, 255, 255],
+    address: [0, 1, 0, 0, 0, 0, 0, 0,
+              0, 0, 0, 1, 0, 0, 0, 0],
+    netmask: [255, 255, 255, 255, 0, 0, 0, 0,
+              0, 0, 0, 0, 0, 0, 0, 0],
+    wildcard: [0, 0, 0, 0, 255, 255, 255, 255,
+               255, 255, 255, 255, 255, 255, 255, 255],
+    broadcast: [0, 1, 0, 0, 255, 255, 255, 255,
+                255, 255, 255, 255, 255, 255, 255, 255],
 }];
 
 
 describe("Network6", function() {
     describe("constructor", function() {
         it("should accept valid values", function() {
-            IPV6_NETWORKS_VALID.forEach((data) => {
-                const net = new Network6(data.input);
-            });
+            assert.isOk(new Network6(IPV6_NETWORKS_VALID[0].input));
         });
     });
-    it("should have the correct types", function() {
-        IPV6_NETWORKS_VALID.forEach((data) => {
-            const net = new Network6(data.input);
-            assert(net instanceof Network);
-            assert(net.getAddress() instanceof Address6);
-            assert(net.getNetmask() instanceof Address6);
-            assert(net.getWildcard() instanceof Address6);
-            assert(net.getBroadcast() instanceof Address6);
-        });
+    it("should have the Network type", function() {
+        assert.instanceOf(new Network6(IPV6_NETWORKS_VALID[0].input), Network);
     });
-    it("should match network address", function() {
-        IPV6_NETWORKS_VALID.forEach((data) => {
-            const net = new Network6(data.input);
-            assert.equal(net.getAddress().getIpString(), data.stringAddress);
-            assert.deepEqual(net.getAddress().getOctets(), data.octetsAddress);
-        });
+    it("address should have the Address6 type", function() {
+        assert.instanceOf(new Network6(IPV6_NETWORKS_VALID[0].input).getAddress(), Address6);
     });
-    it("should match netmask address", function() {
-        IPV6_NETWORKS_VALID.forEach((data) => {
-            const net = new Network6(data.input);
-            assert.equal(net.getNetmask().getIpString(), data.stringNetmask);
-            assert.deepEqual(net.getNetmask().getOctets(), data.octetsNetmask);
-        });
+    it("netmask should have the Address6 type", function() {
+        assert.instanceOf(new Network6(IPV6_NETWORKS_VALID[0].input).getNetmask(), Address6);
     });
-    it("should match prefix", function() {
-        IPV6_NETWORKS_VALID.forEach((data) => {
-            const net = new Network6(data.input);
-            assert.equal(net.getPrefix(), data.prefix);
-        });
+    it("wildcard should have the Address6 type", function() {
+        assert.instanceOf(new Network6(IPV6_NETWORKS_VALID[0].input).getWildcard(), Address6);
     });
-    it("should match wildcard address", function() {
-        IPV6_NETWORKS_VALID.forEach((data) => {
-            const net = new Network6(data.input);
-            assert.equal(net.getWildcard().getIpString(), data.stringWildcard);
-            assert.deepEqual(net.getWildcard().getOctets(), data.octetsWildcard);
-        });
+    it("broadcast should have the Address6 type", function() {
+        assert.instanceOf(new Network6(IPV6_NETWORKS_VALID[0].input).getBroadcast(), Address6);
     });
-    it("should match broadcast address", function() {
-        IPV6_NETWORKS_VALID.forEach((data) => {
-            const net = new Network6(data.input);
-            assert.equal(net.getBroadcast().getIpString(), data.stringBroadcast);
-            assert.deepEqual(net.getBroadcast().getOctets(), data.octetsBroadcast);
+    IPV6_NETWORKS_VALID.forEach((data) => {
+        it(`${data.input} should match network address ${data.address}`, function() {
+            assert.deepEqual(new Network6(data.input).getAddress().getOctets(), data.address);
         });
-    });
-    /*
-    it("should match number of addresses", function() {
-        IPV6_NETWORKS_VALID.forEach((data) => {
-            const net = new Network6(data.input);
-            assert.equal(net.getNumAddresses(), 0);
+        it(`${data.input} should match netmask address ${data.netmask}`, function() {
+            assert.deepEqual(new Network6(data.input).getNetmask().getOctets(), data.netmask);
         });
-    });
-    */
-    it("hosts should start with the first address", function() {
-        IPV6_NETWORKS_VALID.forEach((data) => {
-            const net = new Network6(data.input);
-            assert.equal(
-                net.hosts().next().value.getIpString(),
-                new Address6(data.octetsAddress).add([1]).getIpString(),
-            );
+        it(`${data.input} should match wildcard address ${data.wildcard}`, function() {
+            assert.deepEqual(new Network6(data.input).getWildcard().getOctets(), data.wildcard);
         });
-    });
-    it("hosts should end with the last address", function() {
-        IPV6_NETWORKS_VALID.forEach((data) => {
-            const net = new Network6(data.input);
-            assert(net.hosts(new Address6(data.octetsBroadcast).substract([1])).next().done);
+        it(`${data.input} should match broadcast address ${data.broadcast}`, function() {
+            assert.deepEqual(new Network6(data.input).getBroadcast().getOctets(), data.broadcast);
         });
-    });
-    it("should contains the first address", function() {
-        IPV6_NETWORKS_VALID.forEach((data) => {
-            const net = new Network6(data.input);
-            assert(net.contains(new Address6(data.octetsAddress).add([1])));
-        });
-    });
-    it("should contains the last address", function() {
-        IPV6_NETWORKS_VALID.forEach((data) => {
-            const net = new Network6(data.input);
-            assert(net.contains(new Address6(data.octetsBroadcast).substract([1])));
-        });
-    });
-    it("should NOT contains before the first address", function() {
-        IPV6_NETWORKS_VALID.forEach((data) => {
-            const net = new Network6(data.input);
-            assert(!net.contains(new Address6(data.octetsAddress).substract([1])));
-        });
-    });
-    it("should NOT contains after the last address", function() {
-        IPV6_NETWORKS_VALID.forEach((data) => {
-            const net = new Network6(data.input);
-            assert(!net.contains(new Address6(data.octetsBroadcast).add([1])));
+        it(`${data.input} should match prefix ${data.prefix}`, function() {
+            assert.equal(new Network6(data.input).getPrefix(), data.prefix);
         });
     });
 });
