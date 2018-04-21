e37cea0caebbad0fea29d9c8877ce417066dc3e9
Terseus <terseus@gmail.com>
2018-04-21 08:56:11 +0200

    Make Network4 tests single-assert

--- tests/network4.ts
+++ tests/network4.ts
@@ -1,4 +1,4 @@
-import * as assert from "assert";
+import { assert } from "chai";
 import {
     Address4,
     Network,
@@ -9,163 +9,128 @@ import {
 interface NetworkInfo {
     inputPrefix: string;
     inputAddress: string;
-    stringAddress: string;
-    octetsAddress: number[];
+    address: number[];
     prefix: number;
-    stringNetmask: string;
-    octetsNetmask: number[];
-    stringWildcard: string;
-    octetsWildcard: number[];
-    stringBroadcast: string;
-    octetsBroadcast: number[];
+    netmask: number[];
+    wildcard: number[];
+    broadcast: number[];
     numAddresses: number;
+    firstAddress: number[];
+    lastAddress: number[];
+    beforeFirstAddress: number[];
+    afterLastAddress: number[];
 }
 
 /* tslint:disable:object-literal-sort-keys */
 const IPV4_NETWORKS_VALID: NetworkInfo[] = [{
     inputAddress: "192.168.0.0/255.255.255.0",
     inputPrefix: "192.168.0.0/24",
-    stringAddress: "192.168.0.0",
-    octetsAddress: [192, 168, 0, 0],
+    address: [192, 168, 0, 0],
     prefix: 24,
-    stringNetmask: "255.255.255.0",
-    octetsNetmask: [255, 255, 255, 0],
-    stringWildcard: "0.0.0.255",
-    octetsWildcard: [0, 0, 0, 255],
-    stringBroadcast: "192.168.0.255",
-    octetsBroadcast: [192, 168, 0, 255],
+    netmask: [255, 255, 255, 0],
+    wildcard: [0, 0, 0, 255],
+    broadcast: [192, 168, 0, 255],
     numAddresses: 256,
+    firstAddress: [192, 168, 0, 1],
+    lastAddress: [192, 168, 0, 254],
+    beforeFirstAddress: [192, 167, 255, 255],
+    afterLastAddress: [192, 168, 1, 0],
 }, {
     inputAddress: "10.0.0.0/255.0.0.0",
     inputPrefix: "10.0.0.0/8",
-    stringAddress: "10.0.0.0",
-    octetsAddress: [10, 0, 0, 0],
+    address: [10, 0, 0, 0],
     prefix: 8,
-    stringNetmask: "255.0.0.0",
-    octetsNetmask: [255, 0, 0, 0],
-    stringWildcard: "0.255.255.255",
-    octetsWildcard: [0, 255, 255, 255],
-    stringBroadcast: "10.255.255.255",
-    octetsBroadcast: [10, 255, 255, 255],
+    netmask: [255, 0, 0, 0],
+    wildcard: [0, 255, 255, 255],
+    broadcast: [10, 255, 255, 255],
     numAddresses: 16777216,
+    firstAddress: [10, 0, 0, 1],
+    lastAddress: [10, 255, 255, 254],
+    beforeFirstAddress: [9, 255, 255, 255],
+    afterLastAddress: [11, 0, 0, 0],
 }, {
     inputAddress: "148.56.0.0/255.255.240.0",
     inputPrefix: "148.56.0.0/20",
-    stringAddress: "148.56.0.0",
-    octetsAddress: [148, 56, 0, 0],
+    address: [148, 56, 0, 0],
     prefix: 20,
-    stringNetmask: "255.255.240.0",
-    octetsNetmask: [255, 255, 240, 0],
-    stringWildcard: "0.0.15.255",
-    octetsWildcard: [0, 0, 15, 255],
-    stringBroadcast: "148.56.15.255",
-    octetsBroadcast: [148, 56, 15, 255],
+    netmask: [255, 255, 240, 0],
+    wildcard: [0, 0, 15, 255],
+    broadcast: [148, 56, 15, 255],
     numAddresses: 4096,
+    firstAddress: [148, 56, 0, 1],
+    lastAddress: [148, 56, 15, 254],
+    beforeFirstAddress: [148, 55, 0, 0],
+    afterLastAddress: [148, 56, 16, 0],
 }, {
     inputAddress: "192.168.0.40/255.255.248.0",
     inputPrefix: "192.168.0.40/21",
-    stringAddress: "192.168.0.0",
-    octetsAddress: [192, 168, 0, 0],
+    address: [192, 168, 0, 0],
     prefix: 21,
-    stringNetmask: "255.255.248.0",
-    octetsNetmask: [255, 255, 248, 0],
-    stringWildcard: "0.0.7.255",
-    octetsWildcard: [0, 0, 7, 255],
-    stringBroadcast: "192.168.7.255",
-    octetsBroadcast: [192, 168, 7, 255],
+    netmask: [255, 255, 248, 0],
+    wildcard: [0, 0, 7, 255],
+    broadcast: [192, 168, 7, 255],
     numAddresses: 2048,
+    firstAddress: [192, 168, 0, 1],
+    lastAddress: [192, 168, 7, 254],
+    beforeFirstAddress: [192, 167, 255, 255],
+    afterLastAddress: [192, 168, 8, 0],
 }];
 
 
 function describeTestNetwork(describeSuffix: string, inputAccessor: (data: NetworkInfo) => string) {
     describe(`public interface ${describeSuffix}`, function() {
-        it("should have the correct types", function() {
-            IPV4_NETWORKS_VALID.forEach((data) => {
-                const net = new Network4(inputAccessor(data));
-                assert(net instanceof Network);
-                assert(net.getAddress() instanceof Address4);
-                assert(net.getNetmask() instanceof Address4);
-                assert(net.getWildcard() instanceof Address4);
-                assert(net.getBroadcast() instanceof Address4);
-            });
+        it("should have the Network type", function() {
+            assert.instanceOf(new Network4(inputAccessor(IPV4_NETWORKS_VALID[0])), Network);
         });
-        it("should match network address", function() {
-            IPV4_NETWORKS_VALID.forEach((data) => {
-                const net = new Network4(inputAccessor(data));
-                assert.equal(net.getAddress().getIpString(), data.stringAddress);
-                assert.deepEqual(net.getAddress().getOctets(), data.octetsAddress);
-            });
+        it("address should have the Address4 type", function() {
+            assert.instanceOf(new Network4(inputAccessor(IPV4_NETWORKS_VALID[0])).getAddress(), Address4);
         });
-        it("should match netmask address", function() {
-            IPV4_NETWORKS_VALID.forEach((data) => {
-                const net = new Network4(inputAccessor(data));
-                assert.equal(net.getNetmask().getIpString(), data.stringNetmask);
-                assert.deepEqual(net.getNetmask().getOctets(), data.octetsNetmask);
-            });
+        it("netmask should have the Address4 type", function() {
+            assert.instanceOf(new Network4(inputAccessor(IPV4_NETWORKS_VALID[0])).getNetmask(), Address4);
         });
-        it("should match prefix", function() {
-            IPV4_NETWORKS_VALID.forEach((data) => {
-                const net = new Network4(inputAccessor(data));
-                assert.equal(net.getPrefix(), data.prefix);
-            });
+        it("wildcard should have the Address4 type", function() {
+            assert.instanceOf(new Network4(inputAccessor(IPV4_NETWORKS_VALID[0])).getWildcard(), Address4);
         });
-        it("should match wildcard address", function() {
-            IPV4_NETWORKS_VALID.forEach((data) => {
-                const net = new Network4(inputAccessor(data));
-                assert.equal(net.getWildcard().getIpString(), data.stringWildcard);
-                assert.deepEqual(net.getWildcard().getOctets(), data.octetsWildcard);
-            });
+        it("broadcast should have the Address4 type", function() {
+            assert.instanceOf(new Network4(inputAccessor(IPV4_NETWORKS_VALID[0])).getBroadcast(), Address4);
         });
-        it("should match broadcast address", function() {
-            IPV4_NETWORKS_VALID.forEach((data) => {
-                const net = new Network4(inputAccessor(data));
-                assert.equal(net.getBroadcast().getIpString(), data.stringBroadcast);
-                assert.deepEqual(net.getBroadcast().getOctets(), data.octetsBroadcast);
+        IPV4_NETWORKS_VALID.forEach((input) => {
+            const inputString = inputAccessor(input);
+            it(`${inputString} should match network address ${input.address}`, function() {
+                assert.deepEqual(new Network4(inputString).getAddress().getOctets(), input.address);
             });
-        });
-        it("should match number of addresses", function() {
-            IPV4_NETWORKS_VALID.forEach((data) => {
-                const net = new Network4(inputAccessor(data));
-                assert.equal(net.getNumAddresses(), data.numAddresses);
+            it(`${inputString} should match netmask address ${input.netmask}`, function() {
+                assert.deepEqual(new Network4(inputString).getNetmask().getOctets(), input.netmask);
             });
-        });
-        it("hosts should start with the first address", function() {
-            IPV4_NETWORKS_VALID.forEach((data) => {
-                const net = new Network4(inputAccessor(data));
-                assert.equal(
-                    net.hosts().next().value.getIpString(),
-                    new Address4(data.octetsAddress).add([1]).getIpString(),
-                );
+            it(`${inputString} should match wildcard address ${input.wildcard}`, function() {
+                assert.deepEqual(new Network4(inputString).getWildcard().getOctets(), input.wildcard);
             });
-        });
-        it("hosts should end with the last address", function() {
-            IPV4_NETWORKS_VALID.forEach((data) => {
-                const net = new Network4(inputAccessor(data));
-                assert(net.hosts(new Address4(data.octetsBroadcast).substract([1])).next().done);
+            it(`${inputString} should match broadcast address ${input.broadcast}`, function() {
+                assert.deepEqual(new Network4(inputString).getBroadcast().getOctets(), input.broadcast);
             });
-        });
-        it("should contains the first address", function() {
-            IPV4_NETWORKS_VALID.forEach((data) => {
-                const net = new Network4(inputAccessor(data));
-                assert(net.contains(new Address4(data.octetsAddress).add([1])));
+            it(`${inputString} should match prefix ${input.prefix}`, function() {
+                assert.equal(new Network4(inputString).getPrefix(), input.prefix);
             });
-        });
-        it("should contains the last address", function() {
-            IPV4_NETWORKS_VALID.forEach((data) => {
-                const net = new Network4(inputAccessor(data));
-                assert(net.contains(new Address4(data.octetsBroadcast).substract([1])));
+            it(`${inputString} should match number of addresses ${input.numAddresses}`, function() {
+                assert.equal(new Network4(inputString).getNumAddresses(), input.numAddresses);
             });
-        });
-        it("should NOT contains before the first address", function() {
-            IPV4_NETWORKS_VALID.forEach((data) => {
-                const net = new Network4(inputAccessor(data));
-                assert(!net.contains(new Address4(data.octetsAddress).substract([1])));
+            it(`${inputString} should start with the first address ${input.firstAddress}`, function() {
+                assert.deepEqual(new Network4(inputString).hosts().next().value.getOctets(), input.firstAddress);
             });
-        });
-        it("should NOT contains after the last address", function() {
-            IPV4_NETWORKS_VALID.forEach((data) => {
-                const net = new Network4(inputAccessor(data));
-                assert(!net.contains(new Address4(data.octetsBroadcast).add([1])));
+            it(`${inputString} should end with the last address ${input.lastAddress}`, function() {
+                assert.isTrue(new Network4(inputString).hosts(new Address4(input.lastAddress)).next().done);
+            });
+            it(`${inputString} should contains the first address ${input.firstAddress}`, function() {
+                assert.isTrue(new Network4(inputString).contains(new Address4(input.firstAddress)));
+            });
+            it(`${inputString} should contains the last address ${input.lastAddress}`, function() {
+                assert.isTrue(new Network4(inputString).contains(new Address4(input.lastAddress)));
+            });
+            it(`${inputString} should not contains before the first address ${input.beforeFirstAddress}`, function() {
+                assert.isFalse(new Network4(inputString).contains(new Address4(input.beforeFirstAddress)));
+            });
+            it(`${inputString} should not contains after the last address ${input.afterLastAddress}`, function() {
+                assert.isFalse(new Network4(inputString).contains(new Address4(input.afterLastAddress)));
             });
         });
     });
@@ -174,17 +139,13 @@ function describeTestNetwork(describeSuffix: string, inputAccessor: (data: Netwo
 
 
 describe("Network4", function() {
-    describe("constructor with prefix netmask", function() {
-        it("should accept valid values", function() {
-            IPV4_NETWORKS_VALID.forEach((data) => {
-                const net = new Network4(data.inputPrefix);
+    describe("constructor", function() {
+        IPV4_NETWORKS_VALID.forEach((input) => {
+            it(`should accept valid prefix value ${input.inputPrefix}`, function() {
+                assert.isOk(new Network4(input.inputPrefix));
             });
-        });
-    });
-    describe("constructor with address netmask", function() {
-        it("should accept valid values", function() {
-            IPV4_NETWORKS_VALID.forEach((data) => {
-                const net = new Network4(data.inputAddress);
+            it(`should accept valid netmask value ${input.inputAddress}`, function() {
+                assert.isOk(new Network4(input.inputAddress));
             });
         });
     });
